abdea8303b87f281f2cd8044dafa68ac7a607019
TDev <thientran98qb@gmail.com>
2024-01-10 17:02:53 +0700

    Login with email pass jwt

--- src/repositories/user.ts
+++ src/repositories/user.ts
@@ -1,12 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 import { inject, injectable } from "tsyringe";
 import { convertToNexusType } from "../common/prismaUtils";
 import { NexusGenInputs, NexusGenRootTypes } from "../generated/nexus-typegen";
-import { hashPassword } from "../common/bcrypt";
+import { comparePassword, hashPassword } from "../common/bcrypt";
+import { AuthenticationError } from "apollo-server-errors";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/enviroment";
 export interface IUserRepository {
   createUser(
     input: NexusGenInputs["RegisterInput"]
   ): Promise<Omit<NexusGenRootTypes["User"], "password">>;
+
+  loginUser(
+    input: NexusGenInputs["LoginInput"]
+  ): Promise<NexusGenRootTypes["Auth"]>;
 }
 
 @injectable()
@@ -26,4 +33,38 @@ export default class UserRepository implements IUserRepository {
 
     return res && convertToNexusType.User(res);
   }
+
+  async loginUser(
+    input: NexusGenInputs["LoginInput"]
+  ): Promise<NexusGenRootTypes["Auth"]> {
+    const findUser = await this.prisma.user.findFirst({
+      where: {
+        email: input.email,
+      },
+    });
+    if (!findUser) {
+      throw new AuthenticationError("User is not exist in system!");
+    }
+
+    if (!(await comparePassword(input.password, findUser.password))) {
+      throw new AuthenticationError("Password is wrong !");
+    }
+
+    return this.getUserToken(findUser);
+  }
+
+  private getUserToken = (user: User) => {
+    const accessToken = sign(
+      {
+        user_id: user.id,
+        email: user.email,
+      },
+      config.JWT_TOKEN
+    );
+
+    return {
+      accessToken,
+      user: convertToNexusType.User(user),
+    };
+  };
 }
